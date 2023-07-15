32892ec297ff9babc4d6b47c08302214f23dffb1
Timofeev Aleksandr <115435770+TimofeevAleksandrIVT32@users.noreply.github.com>
2023-07-15 17:11:04 +0300

    main.c

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
 #include "archive_definition.h"
 
 int main(int argc, char *argv[]) {
-    int size[argc - 1];
-    unsigned char **data = read_data(argc - 1, argv, size);
-    if (data == NULL) {
+    int res = read_data(argc - 1, argv);
+    if (res != 0) {
 	    exit(1);
     }
-    arch_def(argc - 1, data, argv, size);
-    free_mem(argc - 1, data);
     return 0;
 }
